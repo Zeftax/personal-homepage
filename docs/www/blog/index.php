62d993e35a1c359fcfdcc6f0f3f4899f64318926
Zdeněk Borovec <zdenda.borovec@gmail.com>
2024-01-13 06:45:19 +0100

    added a blogpost

--- docs/www/blog/index.php
+++ docs/www/blog/index.php
@@ -8,9 +8,23 @@ include_once($COMMONS."/header.php");
 
 <article>
 <p>
-	blogs will be written here, at some point in the future.
+	Blogs will be written here, at some point in the future.
+</p>
+<p>
+	The way in which they are made and addressed right now is temporary, so any links you save / share WILL EXPIRE eventualy. This includes media.
 </p>
 </article>
+<hr/>
+<ul>
+	<li><div><a href="http://www.zdenekborovec-dev.cz/blog/godot_spell_system_prototype">
+		<h3>
+			Godot spell casting system prototype
+		</h3>
+		<p>
+			I have wanted to implement this system in my upcoming game "The Labyrith II" for a few years now, but I got distracted and left it, well, I am back now.
+		</p>
+	</a></div></li>
+</ul>
 
 <?php
 include_once($COMMONS."/footer.php");

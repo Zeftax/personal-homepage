008a9a51d25f5e0b0099558b6503b5ff894008a5
Zdeněk Borovec <zdenda.borovec@gmail.com>
2024-01-19 03:54:41 +0100

    refactor

--- docs/common/header.php
+++ docs/common/header.php
@@ -2,7 +2,7 @@
 
 <html lang="en">
 	<head>
-		<meta http-equiv="content-type" content="text/html; charset=UTF-8;">
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 		<link rel="icon" href="http://www.zdenekborovec-dev.cz/favicon.ico">
 		<link rel="stylesheet" href="http://assets.zdenekborovec-dev.cz/common/styles.css?1703173954">
 		<title><?= isset($PageTitle) ? $PageTitle : "Zdenek Home Page"?></title>

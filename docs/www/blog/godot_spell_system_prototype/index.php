213a6a9b29442791f62ff7901931aaec06026814
Zdeněk Borovec <zdenda.borovec@gmail.com>
2024-01-19 03:14:06 +0100

    prettified the blog css

--- docs/www/blog/godot_spell_system_prototype/index.php
+++ docs/www/blog/godot_spell_system_prototype/index.php
@@ -11,8 +11,19 @@ include_once($COMMONS."/header.php");
 	<h1>
 		Godot spell casting system prototype.
 	</h1>
-	<div style="text-align:right">
-		Published on: 2024-01-13
+	<div class="blog-metadata">
+		<span class="blog-tag" style="background-color: cornflowerblue">
+			godot
+		</span>
+		<span class="blog-tag" style="background-color: khaki">
+			game-dev
+		</span>
+		<span class="blog-tag" style="background-color: sienna">
+			the-labyrinth-II
+		</span>
+		<span class="blog-publish-date">
+			Published on: 2024-01-13
+		</span>
 	</div>
 	<p>
 		I wanted to implement this system in my game "The Labyrinth II" ever since I was maybe 13, but the project failed a few times and for the last few years it has been just rotting in the back of my mind. Well, I have decided to return to it again.
@@ -24,44 +35,52 @@ include_once($COMMONS."/header.php");
 		Now, I have to admit, that I am not at all sure if this system will actually work in a modern game, or, really any game that's not basically turn-based. I do have some ideas how to solve these problems (including a time slow-down mechanic), but that is too far in the future now. I am planning on taking this very easy and slow, more focused on having fun and exploring the godot engine, rather than trying to sprint to the goal of a finished game.
 	</p>
 
-	<h3> Gallery </h3>
-	<div>
-		<img src="http://www.zdenekborovec-dev.cz/blog/godot_spell_system_prototype/nodes.png"/>
-		<p>
-			A path drawn between some nodes, this is how the spell casting looks like.
-		</p>
-		<img src="http://www.zdenekborovec-dev.cz/blog/godot_spell_system_prototype/console.png"/>
-		<p>
-			A log from the spellbook, it detects all entered paths. It also has a dictionary of paths and spells, if you enter a path that corresponds to a spell, you cast it!
-		</p>
-		<img src="http://www.zdenekborovec-dev.cz/blog/godot_spell_system_prototype/spellbookexports.png"/>
-		<p>
-			This is how I can interact with the spellbook from the editor. I can add in nodes (it then works with them automagically, so I am not anyhow limited to the 3x3 grid). And I can also add in the spell paths and names (I can then use these to emit a spell_cast event with its name, or something like that).
-		</p>
+	<div class="image-gallery">
+		<h2> Gallery </h2>
+		<div>
+			<div>
+				<img src="http://www.zdenekborovec-dev.cz/blog/godot_spell_system_prototype/nodes.png"/>
+				<p>
+					A path drawn between some nodes, this is what the spell casting looks like.
+				</p>
+			</div>
+			<div>
+				<img src="http://www.zdenekborovec-dev.cz/blog/godot_spell_system_prototype/console.png"/>
+				<p>
+					A log from the spellbook, it detects all entered paths. It also has a dictionary of paths and spells, if you enter a path that corresponds to a spell, you cast it!
+				</p>
+			</div>
+			<div>
+				<img src="http://www.zdenekborovec-dev.cz/blog/godot_spell_system_prototype/spellbookexports.png"/>
+				<p>
+					This is how I can interact with the spellbook from the editor. I can add in nodes (it then works with them automagically, so I am not anyhow limited to the 3x3 grid). And I can also add in the spell paths and names (I can then use these to emit a spell_cast event with its name, or something like that).
+				</p>
+			</div>
+		</div>
 	</div>
 
 	<p>
-		I am not including any code snippets or more technical stuff as I do not have the css for any code blocks or galleries of pictures (lets be honest, the one right above this text looks absolutely disgusting, or at least it looks like that while I am writing this, I will elaborate on the meta problems of this blog in the next paragraph). But please do let me know if you want to see more or less of the more technical stuff, I am listening (or rather reading e-mails ;-)).
+		Once I tried actually using it in a control node, I found out the mapping of the mouse was completely off, I remade it into control nodes, which I am actually happy about as I feel like the nodes being buttons makes more sense than them being sprites with a collision box that only reacts to the mouse, or whatever I was using, I don't wven remember, but it was wacky :D. But I encountered a problem! I still had to use the Line2D, which is a Node2D. And it's position just did not map well. Through experimentation I found out that it was using the position as relating to its scene origin, but once the control scene was inserted into a control container, it got scaled, as controls should, but it was now offset and the line was not. The only solution I could find was putting the SpellBook scene inside of a MarginContainer, and offseting it using margins and thus preserving its 'relative origin', instead of letting it stretch. I do not like this solution as it requires manual fiddling if you want to change up the HUD, but it was the only solution I could find.
 	</p>
 	<p>
-		So, the promised meta rambling. These blogposts are, in their current form, very temporary. I am just writing it without any express purpose to see what kind of template I might want for a blogpost. The lacking css is just me not wanting to do css at the time of writing. Right now I am just collecting the need for such things as the article content, header, short summary, publish date, previous and next blogpost in related series, and so on and so forth. Ah, hell, let me just throw a code snippet in here to test the css on in the future.
+		Here is a bit of code, so that I can test my code block css:
 	</p>
-	<div>
-		# Called when the node enters the scene tree for the first time.
-		func _ready():
-			if(spell_nodes.size() == 0):
-				print("feedbacking")
-				fallback_add_nodes()
-			for spellNode in spell_nodes:
-				spellNode.input_event.connect(spell_node_input_event)
-	</div>
+	<code>
+		<pre>
+## Add a node to current spell
+func _on_spellnode_entered(node: SpellNode)-&gt;void:
+	if is_receiving_spell &amp;&amp; not spell_builder.has(node):
+	# Append node to the spell builder
+	spell_builder.append(node);
+
+		# Add new point to the spell line and set the old top to the new nodes position
+		spell_line.add_point(node.position)
+		spell_line.set_point_position(spell_line.get_point_count() - 2, node.position)
+		</pre>
+	</code>
 	<p>
-		Damn, just look how much styling that needs.
+		I would like to add some kind of syntax highlighting, but from my research I'd have to write a php function for that, as css cannot look for key words, and writing a bunch of php is not something I am currently in the mood for, so this will have to do, for now.
 	</p>
-	<p>
-		Also, have som grass texture I made, but most likely won't use. But I think it's pretty so I don't want to delete it.
-	</a>
-		<img src="http://www.zdenekborovec-dev.cz/blog/godot_spell_system_prototype/grass.png"/>
 </article>
 
 <?php
